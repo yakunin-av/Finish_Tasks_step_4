b4905ff7593870fbc6f736bce9839b64f7b9b1a7
yakunin-av <81006693+yakunin-av@users.noreply.github.com>
2021-08-11 23:21:51 +0300

    New test methods were added.

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,31 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+
+def test_guest_can_see_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_guest_can_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_guest_can_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
