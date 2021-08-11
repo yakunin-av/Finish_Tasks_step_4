cacfb777c6fe07b7bb994f2bf06e265dc6e1dbaf
yakunin-av <81006693+yakunin-av@users.noreply.github.com>
2021-08-12 01:20:06 +0300

    Options for switching between pages

--- test_main_page.py
+++ test_main_page.py
@@ -7,13 +7,11 @@ def test_guest_can_see_login_url(browser):
     page.open()
     page.should_be_login_url()
 
-
 def test_guest_can_see_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_form()
-
 
 def test_guest_can_see_register_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -21,14 +19,16 @@ def test_guest_can_see_register_form(browser):
     page.open()
     page.should_be_register_form()
 
-
-
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    #login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
+
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
