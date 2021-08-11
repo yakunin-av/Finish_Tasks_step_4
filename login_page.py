d828d87e22ef5e8b26100961f1c9337fb6c7b94d
yakunin-av <81006693+yakunin-av@users.noreply.github.com>
2021-08-12 01:19:47 +0300

    Options for switching between pages

--- login_page.py
+++ login_page.py
@@ -2,12 +2,6 @@ from .base_page import BasePage
 from .locators import LoginPageLocators
 from selenium.webdriver.common.by import By
 
-
-# Структура:
-#  LoginPage:
-#    в should_be_login_url проверяем в assert не равена ли строка self.url через find строке login.  (!= -1)
-#    в shold_be_login_form и register_form ищем через id login_form или register_form в locators
-
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
@@ -16,7 +10,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         assert "login" in self.browser.current_url, "'login' not in current url"
-        
+
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "LOGIN_FORM is not presented"
 
