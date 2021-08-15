e7c9f6bb427dbbd1e9dd0f1d0c10f610bb212713
yakunin-av <81006693+yakunin-av@users.noreply.github.com>
2021-08-16 01:41:30 +0300

    We changed the testing and found a bug in adding the product to the cart.

--- locators.py
+++ locators.py
@@ -7,3 +7,15 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, "#messages div.alertinner strong")
+    PRODUCT_NAME_IN_STORE = (By.CSS_SELECTOR, "#content_inner h1:first-child")
+    PRODUCT_PRICE_BASKET = (By.CSS_SELECTOR, ".alert-info p:first-child strong")
+    PRODUCT_PRICE_STORE = (By.CSS_SELECTOR, "#content_inner p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div.alertinner")
+
+class BasketPageLocators():
+    PRODUCT_IN_BASKET = (By.CSS_SELECTOR, "#content_inner h2")
+    EMPTY_BASKET = (By.CSS_SELECTOR, "#content_inner p")
