caad931937f737d8d0f16925f46e0fac6979c6ed
yakunin-av <81006693+yakunin-av@users.noreply.github.com>
2021-08-18 01:41:14 +0300

    Add files via upload

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,8 @@
-from pages.main_page import MainPage
-from pages.login_page import LoginPage
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+import pytest
+
 
 def test_guest_can_see_login_url(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -7,11 +10,13 @@ def test_guest_can_see_login_url(browser):
     page.open()
     page.should_be_login_url()
 
+
 def test_guest_can_see_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_form()
+
 
 def test_guest_can_see_register_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -19,18 +24,26 @@ def test_guest_can_see_register_form(browser):
     page.open()
     page.should_be_register_form()
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)
-    #login_page = page.go_to_login_page()
-    login_page.should_be_login_page()
 
-def test_guest_should_see_login_link(browser):
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = BasketPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.guest_can_go_to_basket_page()
+    page.guest_cant_see_product_in_basket()
+    page.guest_can_see_empty_basket()
+
+
+@pytest.mark.login_guest
+class TestLoginMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
